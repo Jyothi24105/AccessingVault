961eec0d5b241a45d189ced1bc0c917a99a38de3
jyothi <jyothi@192.168.0.15>
2018-02-17 18:11:42 -0500

    First Commit

---
+++ src/main/java/com/info/AccessingVaultApplication.java
@@ -0,0 +1,12 @@
+package com.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingVaultApplication.class, args);
+	}
+}
